c91aad733dd8bd86793cab190f588929e86fefbc
3yxx <127534673+3yxx@users.noreply.github.com>
2026-04-13 14:22:36 +0300

    Add files via upload

---
+++ Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    private float offsetY;
+
+    void Start()
+    {
+        offsetY = 5;
+    }
+
+    void Update()
+    {
+        
+        //transform.position = new Vector3(player.transform.position.x + 10, 5,transform.position.z);
+        
+    }
+}
